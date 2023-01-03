a5a333c2067d06691779a462a55fd4378ebb9376
nagwa52 <nagwatalaat52@gmail.com>
2023-01-03 16:58:52 +0200

    add logout with dashboard

---
+++ E_commerce_laravel/eshop/resources/views/layouts/admin.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <link rel="stylesheet" type="text/css"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+    <!-- Nucleo Icons -->
+    <link href="admin/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="admin/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Material Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <link href="{{ asset('admin/css/material-dashboard.css?v=3.0.4') }}" rel="stylesheet">
+
+</head>
+
+<body class="g-sidenav-show  bg-gray-200">
+    @include('layouts.inc.sidebar')
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        @include('layouts.inc.adminnav')
+    </main>
+    <div class="container-fluid py-4">
+        @yield('content')
+        @include('layouts.inc.adminfooter')
+    </div>
+    <!-- Scripts -->
+    <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/bootstrap.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/perfect-scrollbar.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/smooth-scrollbar.min.js') }}" defer></script>
+    <script src="{{ asset('admin/js/chartjs.min.js') }}" defer></script>
+    @yield('scripts')
+</body>
+
+</html>
