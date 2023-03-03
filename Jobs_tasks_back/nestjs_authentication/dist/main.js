ee4c8e949c4b7f5aa570ea70c98e528395e366f3
nagwa52 <nagwatalaat52@gmail.com>
2023-03-03 08:17:05 +0200

    add swagger but it doesnt work

--- Jobs_tasks_back/nestjs_authentication/dist/main.js
+++ Jobs_tasks_back/nestjs_authentication/dist/main.js
@@ -4,14 +4,24 @@ const config_1 = require("@nestjs/config");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const logger_service_1 = require("./logger/logger.service");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const configService = app.get(config_1.ConfigService);
     const logger = new logger_service_1.LoggerService();
     app.enableCors();
-    logger.verbose(`Database URI => ${configService.get('database.uri')}`);
-    logger.verbose(`Application listening on port => ${configService.get('port')}`);
-    await app.listen(configService.get('port'));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("Authentication")
+        .setDescription("The Authentication API description")
+        .setVersion("1.0")
+        .addTag("Authentication")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup("api", app, document);
+    logger.verbose(`Database URI => ${configService.get("database.uri")}`);
+    logger.verbose(`Application listening on port => ${configService.get("port")}`);
+    logger.verbose(`Application listening on port => ${configService.get("port")}`);
+    await app.listen(configService.get("port"));
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- Jobs_tasks_back/nestjs_authentication/src/main.ts
+++ Jobs_tasks_back/nestjs_authentication/src/main.ts
@@ -1,7 +1,8 @@
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { LoggerService } from './logger/logger.service';
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { LoggerService } from "./logger/logger.service";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +10,21 @@ async function bootstrap() {
   const logger: LoggerService = new LoggerService();
 
   app.enableCors();
-  logger.verbose(`Database URI => ${configService.get('database.uri')}`);
-  logger.verbose(`Application listening on port => ${configService.get('port')}`);
-  await app.listen(configService.get('port'));
+  const config = new DocumentBuilder()
+    .setTitle("Authentication")
+    .setDescription("The Authentication API description")
+    .setVersion("1.0")
+    .addTag("Authentication")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+  logger.verbose(`Database URI => ${configService.get("database.uri")}`);
+  logger.verbose(
+    `Application listening on port => ${configService.get("port")}`
+  );
+  logger.verbose(
+    `Application listening on port => ${configService.get("port")}`
+  );
+  await app.listen(configService.get("port"));
 }
 bootstrap();
